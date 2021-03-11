606cbcca31c5edc5546a3f445a760b6cf57cd6a3
Fizz Buzz <80414422+fizzbuzz2402@users.noreply.github.com>
2021-03-12 00:37:37 +0530

    Create script.js
    first commit script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function handleClick(){
+  const xhr = new XMLHttpRequest();
+  const url = 'https://jsonplaceholder.typicode.com/todos/1';
+  xhr.open('GET', url);
+  // the headers set in next two line will initaite a preflight call, chrome might have invisible preflight, you may have to turn it on.
+  xhr.setRequestHeader('X-PINGOTHER', 'pingpong');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send();
+}
